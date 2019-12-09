90b66118ddf221a22203f6867a0ea83cbd990a15
pierzbarry <pierz@tribe51>
2019-12-09 11:13:11 -0500

    basic template

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
-import Questions from './components/Questions'
-import Answers from './components/Answers'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 export const Home = () => (
   <div>
@@ -14,15 +7,6 @@ export const Home = () => (
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
     <p>t was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
-    {/* <DndProvider backend={Backend}>
-      <Container>
-        <Row>
-          <Col ><Questions /></Col>
-          <Col ></Col>
-          <Col ><Answers /></Col>
-        </Row>
-      </Container>      
-		</DndProvider> */}
 
   </div>
 )
